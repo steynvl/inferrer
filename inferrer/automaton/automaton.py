dfef89ebe1d10552767657acbe0dfa9b7f308a84
steynvl <steyn.van.l@gmail.com>
2018-06-14 02:53:28 +0200

    Automaton refactoring

--- inferrer/automaton/automaton.py
+++ inferrer/automaton/automaton.py
@@ -1,11 +1,20 @@
 import copy
 from inferrer import utils
+from inferrer.automaton.state import State
 from collections import defaultdict, OrderedDict
 
 
 class Automaton:
 
-    def __init__(self, alphabet, start_state=''):
+    def __init__(self, alphabet: set, start_state: State=State('')):
+        """
+        Initiates a deterministic finite automaton
+
+        :param alphabet: The alphabet of the regular language
+        :type alphabet: set
+        :param start_state: the initial state of the automaton
+        :type start_state: State
+        """
 
         self._start_state = start_state
 
@@ -19,20 +28,83 @@ class Automaton:
 
         self._transitions = defaultdict(OrderedDict)
 
-    def add_transition(self, from_state, to_state, letter):
-        self._transitions[from_state][letter] = to_state
+    def add_transition(self, q1: State, q2: State, a: str):
+        """
+        Adds the transition, delta(q1, a) = q2 to the
+        transition table.
 
-    def transition_exists(self, from_state, letter):
-        return from_state in self._transitions and \
-               letter in self._transitions[from_state] and \
-               self._transitions[from_state][letter] in self._transitions
+        :param q1: from state
+        :type q1: automaton.State
+        :param q2: to state
+        :type q2: automaton.State
+        :param a: letter in alphabet
+        :type a: str
+        """
+        self._transitions[q1][a] = q2
 
-    def perform_transition(self, from_state, letter):
-        if from_state in self._transitions and \
-                letter in self._transitions[from_state]:
-            return self._transitions[from_state][letter]
+    def transition_exists(self, q1: State, a: str) -> bool:
+        """
+        Checks whether the transition
+        delta(q1, a) is defined.
 
-    def find_transition_to_q(self, q):
+        :param q1: from state
+        :type q1: automaton.State
+        :param a: letter in alphabet
+        :type a: str
+        :return: True if it exists
+        :rtype: bool
+        """
+        return q1 in self._transitions and \
+               a in self._transitions[q1] and \
+               self._transitions[q1][a] in self._transitions
+
+    def transition(self, q1: State, a: str) -> bool:
+        """
+        Performs the transition delta(q1, a) and
+        then return the state reached after the
+        transition.
+
+        :param q1: from state
+        :type q1: automaton.State
+        :param a: letter in alphabet
+        :type a: str
+        :return: to state
+        :rtype: automaton.State
+        """
+        if q1 in self._transitions and \
+                a in self._transitions[q1]:
+            return self._transitions[q1][a]
+
+    def parse_string(self, s: str) -> (State, bool):
+        """
+        Parses each character of the input string through
+        the automaton.
+
+        :param s: The string to parse (s element of alphabet*)
+        :type s: str
+        :return: The state after reading the string s and whether
+                 the automaton accepted the input string.
+        :rtype: tuple(State, bool)
+        """
+        q = self._start_state
+        for letter in s:
+            if not self.transition_exists(q, letter):
+                return q, False
+            q = self.transition(q, letter)
+
+        return q, q in self.accept_states
+
+    def find_transition_to_q(self, q: State) -> (State, str):
+        """
+        Finds the State r that satisfies
+        delta(q, a) = r where a is a string in the
+        alphabet
+
+        :param q: Target state
+        :type q: automaton.State
+        :return: The state and whether or not the transition exists
+        :rtype: tuple(State, str)
+        """
         for qf in self._transitions.keys():
             for letter, to_state in self._transitions[qf].items():
                 if to_state == q:
@@ -40,6 +112,14 @@ class Automaton:
         return None, None
 
     def minimize(self):
+        """
+        Minimizes the automaton by removing all
+        states (and transitions) that cannot be
+        reached from the initial state
+
+        :return: the current instance
+        :rtype: self
+        """
         _states = set()
         for state in self.states:
             if state in self._transitions.keys():
@@ -54,6 +134,12 @@ class Automaton:
         return self
 
     def copy(self):
+        """
+        Performs a deep copy of this instance.
+
+        :return: A copied automaton
+        :rtype: Automaton
+        """
         cp = Automaton(self._alphabet)
 
         cp.states = self.states.copy()
@@ -63,55 +149,60 @@ class Automaton:
 
         return cp
 
-    def __deepcopy__(self, memodict={}):
-        return self.copy()
-
     def __str__(self):
+        """
+        ToString implementation for the class, only used
+        for debugging purposes.
+
+        :return: String representation of the dfa
+        :rtype: str
+        """
         rep = [
-            'q     = {}'.format(self._start_state),
-            'Sigma = {}'.format(self._alphabet),
-            'Q     = {}'.format(self.states),
-            'Fa    = {}'.format(self.accept_states),
-            'Fr    = {}'.format(self.reject_states),
+            'Initial state:    = {}'.format(self._start_state),
+            'Alphabet:         = {}'.format(self._alphabet),
+            'States:           = {}'.format(self.states),
+            'Accepting states: = {}'.format(self.accept_states),
+            'Rejecting states: = {}'.format(self.reject_states),
             '\nTransition function: delta'
         ]
 
         for state in sorted(self._transitions.keys()):
             rep.append('state = q_{}'.format(state))
             for letter, to_state in self._transitions[state].items():
-                if to_state in self._transitions:
-                    rep.append('delta(q_{}, {}) = q_{}'.format(state, letter, to_state))
+                rep.append('delta(q_{}, {}) = q_{}'.format(state, letter, to_state))
             rep.append('')
 
         return '\n'.join(rep)
 
-    def parse_string(self, s: str) -> (str, bool):
-        q = self._start_state
-        for letter in s:
-            if not self.transition_exists(q, letter):
-                return q, False
-            q = self.perform_transition(q, letter)
-
-        return q, q in self.accept_states
-
 
 def build_pta(s_plus: set, s_minus: set=set()) -> Automaton:
+    """
+    Function that builds a prefix tree acceptor from the example strings
+    S = S+ union S-
+
+    :param s_plus: Set containing positive examples of the target language
+    :type s_plus: set
+    :param s_minus: Set containing negative examples of the target language
+    :type s_minus: set
+    :return: An automaton representing a prefix tree acceptor
+    :rtype: Automaton
+    """
     samples = s_plus.union(s_minus)
 
     alphabet = utils.determine_alphabet(samples)
     pta = Automaton(alphabet)
 
     for letter in alphabet:
-        pta.add_transition('', letter, letter)
+        pta.add_transition(State(''), letter, letter)
 
     states = {
-        u for u in utils.prefix_set(samples, alphabet)
+        State(u) for u in utils.prefix_set(samples, alphabet)
     }
 
     new_states = set()
     for u in states:
         for a in alphabet:
-            ua = u + a
+            ua = State(u.name + a)
             if ua not in states:
                 new_states.add(ua)
 
@@ -120,9 +211,9 @@ def build_pta(s_plus: set, s_minus: set=set()) -> Automaton:
     states.update(new_states)
 
     for u in states:
-        if u in s_plus:
+        if u.name in s_plus:
             pta.accept_states.add(u)
-        if u in s_minus:
+        if u.name in s_minus:
             pta.reject_states.add(u)
 
     pta.states = states
