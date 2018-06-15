f03b11b15528a1c8d2c2627ce225392950c32d2f
steynvl <steyn.van.l@gmail.com>
2018-06-15 17:13:23 +0200

    Documented utilty functions

--- inferrer/utils/utils.py
+++ inferrer/utils/utils.py
@@ -1,33 +1,88 @@
+"""
+Module containing utility functions such as
+performing operations on strings and combinatorics
+on sets.
+"""
+
 import itertools
+from typing import Set, Generator, Tuple
 
 
-def prefix_set(s, alphabet=None):
+def prefix_set(s: Set[str], alphabet: Set[str]=None) -> Generator[str]:
+    """
+    Calculates the prefix set of the set s given the
+    alphabet.
+
+    :param s: The set fo calculate the prefix set on
+    :type s: Set[str]
+    :param alphabet: Alphabet of the regular language
+    :type alphabet: set
+    :return: Generator with all the prefixes
+    :rtype: Generator[str]
+    """
     return _generate_set(s, alphabet, str.startswith)
 
 
-def suffix_set(s, alphabet=None):
+def suffix_set(s: Set[str], alphabet: Set[str]=None) -> Generator[str]:
+    """
+    Calculates the suffix set of the set s given the
+    alphabet.
+
+    :param s: The set fo calculate the suffix set on
+    :type s: set
+    :param alphabet: Alphabet of the regular language
+    :type alphabet: set
+    :return: Generator with all the suffixes
+    :rtype: Generator[str]
+    """
     return _generate_set(s, alphabet, str.endswith)
 
 
-def determine_alphabet(s):
+def determine_alphabet(s: Set[str]) -> Set[str]:
+    """
+    Calculates the alphabet (Sigma) of the target
+    regular language.
+
+    :param s: Set containing positive and negative
+              example strings of the target regular
+              language.
+    :type s: Set[str]
+    :return: Set containing the unique alphabet of the
+             regular language.
+    :rtype: Set[str]
+    """
     return set(''.join(s))
 
 
-def break_strings_in_two(red):
-    we = set()
-    for r in red:
+def break_strings_in_two(s: str) -> Set[Tuple[str, str]]:
+    """
+    Calculates all combinations of the string
+    split into two parts.
+
+    :param s: string to break in two
+    :type s: str
+    :return: Set of all combinations
+    :rtype: Set[Tuple[str, str]]
+    """
+    combs = set()
+    for r in s:
         if len(r) == 0:
-            we.add(('', ''))
+            combs.add(('', ''))
         elif len(r) == 1:
-            we.update({('', r), (r, '')})
+            combs.update({('', r), (r, '')})
         else:
-            we.update({('', r), (r, '')})
-            for i in range(len(r)):
-                we.add((r[:i], r[i:]))
-    return we
+            combs.update({('', r), (r, '')})
+            combs.update({(r[:i], r[i:]) for i in range(len(r))})
+    return combs
 
 
-def _generate_set(s, alphabet, func):
+def _get_all_combinations(s: set, repeat: int) -> Generator[str]:
+    for rep in range(repeat + 1):
+        for p in itertools.product(s, repeat=rep):
+            yield ''.join(p)
+
+
+def _generate_set(s, alphabet, func) -> Generator[str]:
     if alphabet is None:
         alphabet = determine_alphabet(s)
 
@@ -35,9 +90,3 @@ def _generate_set(s, alphabet, func):
     for comb in _get_all_combinations(alphabet, longest):
         if any([func(i, comb) for i in s]):
             yield comb
-
-
-def _get_all_combinations(s, repeat):
-    for rep in range(repeat + 1):
-        for p in itertools.product(s, repeat=rep):
-            yield ''.join(p)
